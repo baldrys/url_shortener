591cd26b8333190dbe23dce7b82ed6da3dac8b53
baldrys <baldrys@rambler.ru>
2018-01-26 22:33:26 +0300

    Added class based view that can redirect from short url to original one.

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+from .models import ShortURL
+
+
+class ShortURLView(View):
+    def get(self, request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(ShortURL, shortcode=shortcode)
+        return HttpResponseRedirect(obj.url)
--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from shortener.views import ShortURLView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^(?P<shortcode>[\w-]+)/$', ShortURLView.as_view()),
 ]
