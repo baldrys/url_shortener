c746727c4bfdbedd719af144ba8de0a182728d9d
baldrys <baldrys@rambler.ru>
2018-01-30 18:21:27 +0300

    added postgrese db

--- url_shortener/settings/production.py
+++ url_shortener/settings/production.py
@@ -84,6 +84,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -122,6 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 SHORTCODE_MIN = 6
 SHORTCODE_MAX = 15
