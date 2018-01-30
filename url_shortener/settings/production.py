b402ef43f031a7f6cc695893e4c02019b7af5456
baldrys <baldrys@rambler.ru>
2018-01-30 19:06:19 +0300

    changed whitenoise settings

--- url_shortener/settings/production.py
+++ url_shortener/settings/production.py
@@ -51,9 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
 
 ROOT_URLCONF = 'url_shortener.urls'
 
