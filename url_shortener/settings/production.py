607249043db76d04c6d11394c6007342f1d7d934
baldrys <baldrys@rambler.ru>
2018-01-30 17:36:43 +0300

    modified production settings

--- url_shortener/settings/production.py
+++ url_shortener/settings/production.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['www.short-url420.herokuapp.com', 'short-url420.herokuapp.com']
 
-HOST = "short-url420.herokuapp.com"
+HOST = "http://short-url420.herokuapp.com"
 
 # Application definition
 
